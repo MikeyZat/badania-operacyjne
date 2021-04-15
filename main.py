bb53225dfdcc840d5314293acb9493c1fdc35266
ptyszko <62699530+ptyszko@users.noreply.github.com>
2021-04-15 14:08:59 +0200

    Change the model to not use numpy (#2)
    * add gitignore
    
    * Remove numpy references from main.py
    
    * Change Item into an immutable dataclass, remove boilerplate code

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import numpy as np
+import dataclasses
+import random
 
 
 # not sure what this class will look like in the future
+@dataclasses.dataclass(order=True, frozen=True)
 class Item:
-    def __init__(self, weight):
-        self.weight = weight
+    weight: float
+    name: str = dataclasses.field(default='')
 
     def __sub__(self, other):
         return self.weight - other
@@ -12,30 +14,8 @@ class Item:
     def __add__(self, other):
         return self.weight + other
 
-    def __gt__(self, other):
-        return self.weight > other
-
-    def __ge__(self, other):
-        return self.weight >= other
-
-    def __lt__(self, other):
-        return self.weight < other
-
-    def __le__(self, other):
-        return self.weight <= other
-
-    def __iadd__(self, other):
-        self.weight += other
-
-    def __isub__(self, other):
-        self.weight -= other
-
     def __str__(self):
         return str(self.weight)
-
-    def __repr__(self):
-        return str(self.weight)
-
 
 
 def divide_into_trips(items, capacity):
@@ -47,16 +27,18 @@ def divide_into_trips(items, capacity):
         if curr_capacity - item.weight >= 0:
             curr_trip_items.append(item)
         else:
-            divided_weights.append(np.array(curr_trip_items))
+            divided_weights.append(curr_trip_items)
             curr_trip_items = [item]
             curr_capacity = capacity
         curr_capacity -= item.weight
     if curr_trip_items:
-        divided_weights.append(np.array(curr_trip_items))
-    return np.array(divided_weights, dtype=object)
+        divided_weights.append(curr_trip_items)
+    return divided_weights
+
 
 def cost(car_trips, truck_trips, car_cost, truck_cost):
-    return len(car_trips)*car_cost+len(truck_trips)*truck_cost
+    return len(car_trips)*car_cost + len(truck_trips)*truck_cost
+
 
 # I'm assuming every weight < truck capacity
 def rand_solution(items, car_capacity, truck_capacity, car_item_prob=-1):
@@ -64,11 +46,11 @@ def rand_solution(items, car_capacity, truck_capacity, car_item_prob=-1):
     if car_item_prob == -1:
         car_item_prob = car_capacity / truck_capacity
 
-    np.random.shuffle(items)
-    split_i = int(items.shape[0] * car_item_prob)
+    random.shuffle(items)
+    split_i = int(len(items) * car_item_prob)
     car_items = items[:split_i]
-    truck_items = np.concatenate((items[split_i:], car_items[car_items > car_capacity]))
-    car_items = car_items[car_items <= car_capacity]
+    truck_items = items[split_i:] + [item for item in car_items if item.weight > car_capacity]
+    car_items = [item for item in car_items if item.weight <= car_capacity]
 
     return divide_into_trips(car_items, car_capacity), divide_into_trips(truck_items, truck_capacity)
 
@@ -77,23 +59,26 @@ def main():
     n = 10
     car_capacity = n // 2
     truck_capacity = 2 * n
-    weights = np.random.randint(1, n // 1.5, n)
+    weights = random.choices(list(range(1, int(n / 1.5))), k=n)
     
     car_cost = 10
     truck_cost = 50
 
-    items = np.array([Item(w) for w in weights])
+    items = [Item(w) for w in weights]
+    print('all items:')
     print(items)
-    print()
 
     car_trips, truck_trips = rand_solution(items, car_capacity, truck_capacity)
+    print('\nVia car')
     print(car_trips)
-    print("=============")
+    print('\n=============\nVia truck')
     print(truck_trips)
     
     operation_cost = cost(car_trips, truck_trips, car_cost, truck_cost)
-    
+
+    print('\nCost')
     print(operation_cost)
+
 
 if __name__ == '__main__':
     main()
