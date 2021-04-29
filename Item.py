282dfffa8a172096ebffe0ac371616729b9777ae
Mikolaj Zatorski <mikeyzat@gmail.com>
2021-04-29 09:56:16 +0200

    Add genetic algorithm with best rank selection

---
+++ Item.py
@@ -0,0 +1,29 @@
+import dataclasses
+import json
+
+
+@dataclasses.dataclass(order=True, frozen=True)
+class Item:
+    weight: float
+    name: str = dataclasses.field(default='')
+
+    def __sub__(self, other):
+        return self.weight - other
+
+    def __add__(self, other):
+        return self.weight + other
+
+    def __str__(self):
+        return str(self.weight)
+
+    @staticmethod
+    def from_json(path: str):
+        """Wczytuje plik jako JSON i tworzy z niego listę przedmiotów"""
+        with open(path) as fin:
+            ret = json.load(fin)
+        return (
+            [Item(**kwargs) for kwargs in ret['items']],
+            ret['truck_load'], ret['car_load'],
+            ret['truck_cost'], ret['car_cost']
+        )
+
