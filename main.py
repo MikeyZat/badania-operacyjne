5cdf6f816765928ff5b1d9c7bfdbddb9687891d2
ptyszko <62699530+ptyszko@users.noreply.github.com>
2021-05-13 13:40:52 +0200

    Naprawa krzyżowania, CLI (#6)
    * Przykład na którym krzyżowanie się sypie do przebadania
    
    * Update gitignore
    
    * Naprawiona procedura krzyżwania
    
    * Interfejs do zabawy alg. genetycznym
    
    * Rozszerzenie interfejsu, wizualizacja postępu, złapanie 2-go błędu z krzyżowaniem
    
    * Naprawa (?) 2-go błędu krzyżowania
    
    * Naprawa (teraz już prawie na pewno) 2-go błędu krzyżowania

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from GeneticAlgorithm import GeneticAlgorithm
 from genes import Chromosome
 from Item import Item
 from ga_selections import best_rank_selection
+import logging
+from argparse import ArgumentParser
 
 
 def divide_into_trips(items, capacity):
@@ -24,7 +26,7 @@ def divide_into_trips(items, capacity):
 
 
 def cost(car_trips, truck_trips, car_cost, truck_cost):
-    return len(car_trips)*car_cost + len(truck_trips)*truck_cost
+    return len(car_trips) * car_cost + len(truck_trips) * truck_cost
 
 
 # I'm assuming every weight < truck capacity
@@ -37,7 +39,7 @@ def rand_solution(items, car_capacity, truck_capacity, car_item_prob=-1):
     split_i = int(len(items) * car_item_prob)
     car_items = items[:split_i]
     truck_items = items[split_i:] + \
-        [item for item in car_items if item.weight > car_capacity]
+                  [item for item in car_items if item.weight > car_capacity]
     car_items = [item for item in car_items if item.weight <= car_capacity]
 
     return divide_into_trips(car_items, car_capacity), divide_into_trips(truck_items, truck_capacity)
@@ -48,7 +50,7 @@ def print_rand_solution(file_name='ex.json'):
     items, truck_capacity, car_capacity, truck_cost, car_cost = Item.from_json(
         file_name)
     car_trips, truck_trips = rand_solution(
-        items, car_capacity, truck_capacity, random.uniform(0, 1))
+        items, car_capacity, truck_capacity, random.random())
 
     print('\nVia car')
     print(f'n = {len(car_trips)}')
@@ -65,38 +67,67 @@ def print_rand_solution(file_name='ex.json'):
 
 def gene_rand_solution(items, car_capacity, truck_capacity):
     car_trips, truck_trips = rand_solution(
-        items, car_capacity, truck_capacity, random.uniform(0, 1))
+        items, car_capacity, truck_capacity, random.random())
     return car_trips + truck_trips
 
 
-def ga_population_generator(file_name='ex.json'):
+def ga_population_generator(file_name='ex.json', pop_size=100):
     items, truck_capacity, car_capacity, truck_cost, car_cost = Item.from_json(
         file_name)
 
-    return [
-        Chromosome(
-            gene_rand_solution(items, car_capacity, truck_capacity),
-            truck_capacity,
-            car_capacity,
-            truck_cost,
-            car_cost
-        ) for _ in range(100)
-    ]
+    def _gen():
+        return [
+            Chromosome(
+                gene_rand_solution(items, car_capacity, truck_capacity),
+                truck_capacity,
+                car_capacity,
+                truck_cost,
+                car_cost
+            ) for _ in range(pop_size)
+        ]
+
+    return _gen
 
 
-def ga_stop_condition(curr_best_match, curr_best_match_fitness, i):
-    return i > 1000
+def ga_basic_stop_condition(n_gens):
+    def _stop_condition(_, __, i):
+        return i > n_gens
+    return _stop_condition
 
 
 def main():
+    parser = ArgumentParser(
+        description='Demonstracja algorytmów populacyjnych'
+    )
+    parser.add_argument(
+        'infile', nargs='?', default='ex.json',
+        help='Ścieżka do pliku JSON z instancją problemu o strukturze '
+             'identycznej, jak załączony simple.json (domyślnie ex.json)'
+    )
+    parser.add_argument(
+        '-n', dest='gens', type=int, default=1000,
+        help='Ilość pokoleń, które chcemy wygenerować (domyślnie 1000)'
+    )
+    parser.add_argument(
+        '-u', dest='unchanged_gens', type=int, default=200,
+        help='Ilość pokoleń z rzędu bez poprawy, po których powinniśmy '
+             'się zatrzymać (domyślnie 200)'
+    )
+    parser.add_argument(
+        '-k', dest='pop_size', type=int, default=100,
+        help='Rozmiar populacji w każdym pokoleniu'
+    )
+    args = parser.parse_args()
 
-    print_rand_solution()
+    logging.basicConfig(level=logging.ERROR)
+
+    # print_rand_solution()
 
     # GENETIC ALGORITHM STARTS
-    ga = GeneticAlgorithm(ga_population_generator,
-                          best_rank_selection, ga_stop_condition)
+    ga = GeneticAlgorithm(ga_population_generator(args.infile, args.pop_size),
+                          best_rank_selection, ga_basic_stop_condition(args.gens))
 
-    solution = ga.run()
+    solution = ga.run(args.gens)
     print("Found solution:")
     print(solution)
     print(solution.cost)
